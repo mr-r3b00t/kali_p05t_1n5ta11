06e11756025b6eb89cdc5390e436c58f122ebd6d
Daniel Card <dan.card@xservus.com>
2022-03-18 18:05:00 +0000

    Create scratch_kali2022.1.sh

---
+++ scratch_kali2022.1.sh
@@ -0,0 +1,42 @@
+sudo apt-get install python-setuptools
+sudo apt install python3-pip
+pip install shodan
+
+#install axiom
+bash <(curl -s https://raw.githubusercontent.com/pry0cc/axiom/master/interact/axiom-configure)
+
+sudo apt install golang
+
+sudo apt install subfinder
+sudo apt install dnsx
+
+sudo apt install docker-compose -y
+
+sudo apt install nuclei -y
+
+sudo apt install chromium -y
+
+
+go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest
+
+
+#Download ADFIND
+#https://www.joeware.net/downloads/dl2.php
+
+#Download Mimikatz - https://github.com/gentilkiwi/mimikatz/releases
+
+
+#Install Bloodhound
+sudo apt install neo4j
+sudo apt install bloodhound
+
+#run and configure neo4j (default creds are neo4j:neo4j)
+sudo neo4j console
+
+
+#shodan cli
+easy_install shodan
+
+#Run Neclei
+#nuclei 
+
